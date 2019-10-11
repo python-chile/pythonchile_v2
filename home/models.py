704223ea6fbcbbbefa5e64add5ab8b3016cd7bb1
calviciano <cristian@kausana.cl>
2019-10-11 20:23:12 -0300

    Showing menu items on home

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
-from django.db import models
-
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
     pass
+
+    def get_context(self, request, *args, **kwargs):
+        ''' Used for adding menu items to home'''
+        context = super(HomePage, self).get_context(request, *args, **kwargs)
+        context['menuitems'] = self.get_children().filter(
+            live=True, show_in_menus=True)
+
+        return context
