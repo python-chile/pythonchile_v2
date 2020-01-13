400dd6f521724fa629063242fb7e1bc9fbce65a8
calviciano <cristian@kausana.cl>
2020-01-13 19:11:44 -0300

    Gunicorn runner fix

--- pythonchile_v2/configs/pythonchile_runner.sh
+++ pythonchile_v2/configs/pythonchile_runner.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 set -e
-LOGFILE=/opt/pythonchile_env/logs/gunicorn.log
-LOGDIR=/opt/pythonchile_env/logs/
 NAME=pythonchile
 TIMEOUT=90
 BIND='127.0.0.1:9020'
 USER='pythonchile'
-DJANGO_SETTINGS_MODULE=pythonchile_v2.settings_production
-. /opt/pythonchile_env/bin/activate
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+DJANGO_SETTINGS_MODULE=pythonchile_v2.settings.production
 
-exec /opt/pythonchile_env/bin/gunicorn \
---workers 3 \
---user=$USER \
---bind=$BIND \
---log-level=$LOG_LEVEL \
---log-file=$LOGFILE \
-pythonchile_v2.wsgi
+. /opt/pythonchile_env/bin/activate
+cd /opt/pythonchile_env/src
+
+exec /opt/pythonchile_env/bin/gunicorn pythonchile_v2.wsgi --workers 3 --user=$USER --bind=$BIND
