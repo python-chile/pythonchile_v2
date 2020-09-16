b16d5fa8f0416a2d587a99852ce0e24c6c089e74
Cristian Vargas <cristian@inorbit.ai>
2020-09-16 14:51:22 -0300

    Adding database pre-migrations backup

--- update_prod.sh
+++ update_prod.sh
@@ -4,12 +4,11 @@ timestamp() {
   date +"%Y-%m-%d_%H-%M-%S"
 }
 
-echo "$(timestamp)" >> deploys_log.txt
-
 PRODUCTION="/opt/pythonchile_env/src"
 PYENV="/opt/pythonchile_env"
 
 cd $PRODUCTION;
+echo "$(timestamp)" >> deploys_log.txt
 
 echo "- Pulling changes"
 git remote -v
@@ -18,6 +17,9 @@ git log -1 >> deploys_log.txt
 
 echo "- Installing requirements";
 $PYENV/bin/pip install -r $PRODUCTION/requirements/pro.txt || echo "Error installing requirements" >> deploys_log.txt
+
+echo "- Database pre-migrations backup"
+sudo -u postgres bash -c "pg_dump pythonchile_web > ~/backups/pythonchile_web_.$(timestamp).sql;"
 
 echo "- Database migrations";
 $PYENV/bin/python manage.py migrate --settings=pythonchile_v2.settings.production || echo "Error db migrations" >> deploys_log.txt
