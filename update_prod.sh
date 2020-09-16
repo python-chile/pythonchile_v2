924ce14d62f5b0240aded0106b6877a848809c3a
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-09-16 02:06:39 +0200

    Orden al script the update_prod

--- update_prod.sh
+++ update_prod.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# timestamp def
+
 timestamp() {
   date +"%Y-%m-%d_%H-%M-%S"
 }
 
 PRODUCTION="/opt/pythonchile_env/src/"
+PYENV="/opt/pythonchile_env/"
+
 cd $PRODUCTION;
 
 echo "- Pulling changes";
@@ -13,13 +15,13 @@ git pull;
 
 
 echo "- Installing requirements";
-/opt/pythonchile_env/bin/pip install -r /opt/pythonchile_env/src/pythonchile_v2/requirements/pro.txt
+$PYENV/bin/pip install -r $PRODUCTION/pythonchile_v2/requirements/pro.txt
 
 echo "- Database migrations";
-/opt/pythonchile_env/bin/python3.6 manage.py migrate --settings=pythonchile_v2.settings.production
+$PYENV/bin/python3.6 manage.py migrate --settings=pythonchile_v2.settings.production
 
 echo "- Collect Static";
-/opt/pythonchile_env/bin/python3.6 manage.py collectstatic --noinput --link --settings=pythonchile_v2.settings.production
+$PYENV/bin/python3.6 manage.py collectstatic --noinput --link --settings=pythonchile_v2.settings.production
 
 echo "- Restart production";
 /usr/bin/supervisorctl restart pythonchile_web
