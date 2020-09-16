7f1f94de9540a6b4e7f88a8b23d1c03bbfaa886c
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-09-16 19:18:37 +0200

    Update update_prod script

--- update_prod.sh
+++ update_prod.sh
@@ -4,24 +4,26 @@ timestamp() {
   date +"%Y-%m-%d_%H-%M-%S"
 }
 
-PRODUCTION="/opt/pythonchile_env/src/"
-PYENV="/opt/pythonchile_env/"
+echo "$(timestamp)" >> deploys_log.txt
+
+PRODUCTION="/opt/pythonchile_env/src"
+PYENV="/opt/pythonchile_env"
 
 cd $PRODUCTION;
 
-echo "- Pulling changes";
-git remote -v;
-git pull;
-
+echo "- Pulling changes"
+git remote -v
+git pull
+git log -1 >> deploys_log.txt
 
 echo "- Installing requirements";
-$PYENV/bin/pip install -r $PRODUCTION/pythonchile_v2/requirements/pro.txt
+$PYENV/bin/pip install -r $PRODUCTION/requirements/pro.txt || echo "Error installing requirements" >> deploys_log.txt
 
 echo "- Database migrations";
-$PYENV/bin/python3.6 manage.py migrate --settings=pythonchile_v2.settings.production
+$PYENV/bin/python manage.py migrate --settings=pythonchile_v2.settings.production || echo "Error db migrations" >> deploys_log.txt
 
 echo "- Collect Static";
-$PYENV/bin/python3.6 manage.py collectstatic --noinput --link --settings=pythonchile_v2.settings.production
+$PYENV/bin/python manage.py collectstatic --noinput --link --settings=pythonchile_v2.settings.production || echo "Error collect static" >> deploys_log.txt
 
 echo "- Restart production";
-/usr/bin/supervisorctl restart pythonchile_web
+/usr/bin/supervisorctl restart pythonchile_web || echo "Error restart supervisorctl" >> deploys_log.txt
